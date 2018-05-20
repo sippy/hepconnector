59df2d3218ca049fcab220edb98f1ff47905e6ff
Maksym Sobolyev <sobomax@sippysoft.com>
2018-05-20 00:24:05 +0000

    GC unused structure.

--- core_hep.c
+++ core_hep.c
@@ -59,21 +59,6 @@
 
 pthread_mutex_t lock;
 
-#if 0
-static struct hep_ctx ctx = {
-        .initfails = 0,
-        .hints = {{ 0 }},
-        .capt_host  = "10.0.0.1",
-        .capt_port  = "9060",
-        .capt_proto = "udp",
-        .capt_id = 101,
-        .hep_version = 3,
-        .usessl = 0,
-        .pl_compress = 0,
-        .sendPacketsCount = 0
-};
-#endif
-
 #ifdef USE_SSL
 static int initSSL(struct hep_ctx *ctp);
 #endif
