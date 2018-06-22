d8f5d3b014cb8f350aa7d85969a77ce8aac1fc8c
Maksym Sobolyev <sobomax@sippysoft.com>
2018-06-22 10:25:32 -0700

    Reorder includes to hopefully fix implicit declaration of function .getaddrinfo.

--- core_hep.c
+++ core_hep.c
@@ -51,16 +51,16 @@
 
 //#define USE_ZLIB
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <sys/ioctl.h>
 #include <ctype.h>
-#include <sys/socket.h>
 #include <arpa/inet.h>
-#include <netdb.h>
 #include <fcntl.h>
 #include <getopt.h>
 #include <unistd.h>
